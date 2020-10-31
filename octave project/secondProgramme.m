6da8faa39b22a2360dcafc648d03531429b19ee8
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2020-11-01 00:28:11 +0530

    second basic project

---
+++ octave project/secondProgramme.m
@@ -0,0 +1,16 @@
+#Question 02
+A = 105; 
+B = 9.5*1035;
+C = 7+5i;
+D=4-2i;
+E = C-D;
+
+#Question 03
+aVec = [5.3 3.4 6 23];
+bVec = [ 100 6 5.3 3.5];
+cVec = [5 : -0.5 : -5];
+dVec = 'University of Colombo School of Computing'; 
+disp(aVec);
+disp(bVec);
+disp(cVec);
+disp(dVec);
