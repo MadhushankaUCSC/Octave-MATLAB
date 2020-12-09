5e6433271c17c7c5ed7eebe1b8f331a9f4b5b35a
MadhushankaUCSC <buthsaramadhushanka@gmail.com>
2020-12-09 21:56:22 +0530

    inclass assignment question1

---
+++ octave project/inclass assignment/Q1.m
@@ -0,0 +1,11 @@
+stringCocat=['foo';'bar',"huz"]
+size(stringCocat)
+myMovie="I\'am number four"
+
+mat=["| 1 2 3 |";"| 4 5 6 |";"| 7 8 9 |"]
+data=[" a ";"b ";" c"];
+deblank(data)
+strtrim(data)
+
+string="kahatagasdigiliya"
+substr(string,-8,4)
